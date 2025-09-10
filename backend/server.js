21d2767870a267e5d33063e980adbd430f932e86
DarinKhelifa <darinkhelifa@gmail.com>
2025-09-10 19:48:03 +0100

    deployment

--- backend/server.js
+++ backend/server.js
@@ -315,9 +315,8 @@ app.put('/api/update-book/:id', async (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist'))); 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend ','dist', 'index.html'));
-  }
-)
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  });
 }
 
 //Health Check Endpoint
